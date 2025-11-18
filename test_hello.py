8f85f1afdc1818bb065baa70f410e3c779d46e3d
mis1398 <153155136+mis1398@users.noreply.github.com>
2025-11-18 23:58:16 +0800

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,2 +1,7 @@
-def test_ok():
-     assert True 
+# test_hello.py
+def test_always_pass():
+    assert True
+
+def test_hello_string():
+    from hello import hello   # 之後你會有 hello.py 入面有個 function
+    assert hello() == "Hello CI/CD 成功啦！"
